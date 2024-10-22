97e58a0f46a2f8adfee534f0a4405283575de882
hls-sml <hilliassmirali@gmail.com>
2024-10-22 12:10:26 +0200

    2-print_alphabet.c script created

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print alphabet in lower case
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	for ( i = 97; i < 123; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
