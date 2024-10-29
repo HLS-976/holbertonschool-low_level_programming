14de30560d99cce675d0f11df582749cfbd591d0
root <root@hls-sml>
2024-10-29 09:50:01 +0100

    update file

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -15,9 +15,9 @@ void print_diagonal(int n)
 	}
 	else
 	{
-		for (i = 0; i < n; i++)
+		for (i = 1; i <= n; i++)
 		{
-			for (j = 0; j <= i; j++)
+			for (j = 1; j <= i; j++)
 			{
 				if (j == i)
 				{
@@ -30,6 +30,5 @@ void print_diagonal(int n)
 			}
 			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
