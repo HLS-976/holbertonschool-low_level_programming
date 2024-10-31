82d4ba63ed5e5c280d930d60032e914f8c371181
root <root@hls-sml>
2024-10-31 15:07:16 +0100

    puts_half.c file created

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts_half - print half string
+ * @str: parameter
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+	int n = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	len--;
+
+	if (len % 2 != 0)
+	{
+		n = (len + 1) / 2;
+
+		for ( ; n <= len; n++)
+		{
+			_putchar(str[n]);
+		}
+		_putchar('\n');
+	}
+}
