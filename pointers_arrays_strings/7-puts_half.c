1d6dfb4abd6ea6c5c88a3b4d539b63437cd163e3
root <root@hls-sml>
2024-10-31 15:29:57 +0100

    fix

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -7,8 +7,8 @@
  */
 void puts_half(char *str)
 {
-	long int len = 0;
-	long int n = 0;
+	int len = 0;
+	int n = 0;
 
 	while (str[len] != '\0')
 	{
@@ -16,7 +16,7 @@ void puts_half(char *str)
 	}
 	len--;
 
-	if (len % 2 != 0)
+	if ((len % 2) != 0)
 	{
 		n = (len + 1) / 2;
 
