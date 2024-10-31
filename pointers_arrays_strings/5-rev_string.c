f164f6c818e8297dc9ea5a359cb2bab7315b848a
root <root@hls-sml>
2024-10-31 13:49:11 +0100

    create file 5-rev_string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+/**
+ * rev_string - reverse string
+ * @s: string parameter
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+	int j = 0;
+	char tmp_string;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i--;
+
+	while (i != 0)
+	{
+		tmp_string = s[j];
+		s[j] = s[i];
+		s[i] = tmp_string;
+
+		i--;
+		j++;
+	}
+}
