bb5d59905bdd964d7de35dd4f0441d4e6007824f
root <root@hls-sml>
2024-11-03 22:30:25 +0100

    3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -12,7 +12,7 @@ int _strcmp(char *s1, char *s2)
 	while (s1[len] != '\0' && s2[len] != '\0')
 	{
 		if (s1[len] != s2[len])
-			return (s1[len] -s2[len]);
+			return (s1[len] - s2[len]);
 		len++;
 	}
 	return (0);
