06101d0b54f29da34d38ee4fcd2091ca0f1c8083
root <root@hls-sml>
2024-11-03 21:09:59 +0100

    1-strncat.c update

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -9,17 +9,14 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int dest_len = 0;
-	int src_len = 0;
 	int i;
 
 	while (dest[dest_len] != '\0')
 		dest_len++;
 
-	while (src[src_len] != '\0')
-		src_len++;
-
 	for (i = 0; i < n; i++)
 		dest[dest_len + i] = src[i];
+
 
 	return (dest);
 }
