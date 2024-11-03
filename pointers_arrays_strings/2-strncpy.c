dc40f46bfb8ad80e11a3e32575908f520814dc7e
root <root@hls-sml>
2024-11-03 21:45:22 +0100

    2-strncpy.c update code

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -8,17 +8,17 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int len = 0;
 	int i = 0;
 
-	while (src[len] != '\0')
+	while (i < n && src[i] != '\0')
 	{
-		len++;
+		dest[i] = src[i];
+		i++;
 	}
 
 	while  (i < n)
 	{
-		dest[i] = src[i];
+		dest[i] = '\0';
 		i++;
 	}
 
