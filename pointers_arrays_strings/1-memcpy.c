24ab1e6355f25e9188a0385bdbd4e2139f135593
root <root@hls-sml>
2024-11-04 15:01:55 +0100

    1-memcpy.c create file

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+/**
+ * _memcpy - copies memory area
+ * @dest: char
+ * @src: char
+ * @n: unsiggned int
+ * Return: char
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i <= n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
