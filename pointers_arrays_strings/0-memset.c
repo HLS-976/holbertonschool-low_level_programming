853a784b88afea6d2314dbfcea02f731e74c2f58
root <root@hls-sml>
2024-11-04 11:30:42 +0100

    0-memset.c create file

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+/**
+ * _memset - fill memory with constant byte
+ * @s: char
+ * @b: char
+ * @n: unsigned int
+ * Return: char
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
