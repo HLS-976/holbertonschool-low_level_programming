487da8854d201454421aaf403c14314cc350fac2
root <root@hls-sml>
2024-11-05 16:06:40 +0100

    8-print_diagsums.c create file

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints of the two diagonals of a square matrix
+ * @a: int
+ * @size: int
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i;
+
+	int sum1 = 0;
+	int sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += *(a + i * size + i);
+		sum2 += *(a + i * size + (size - i - 1));
+	}
+
+	printf("%d, %d\n", sum1, sum2);
+}
