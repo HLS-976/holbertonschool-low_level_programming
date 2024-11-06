021b89924def5baf29700eec5b8892dad6ed5cf9
root <root@hls-sml>
2024-11-06 13:58:00 +0100

    3-factorial.c create file

---
+++ recursion/3-factorial.c
@@ -0,0 +1,21 @@
+/**
+ * factorial - returns the factorial of a giben number
+ * @n: int
+ * Return: int
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
