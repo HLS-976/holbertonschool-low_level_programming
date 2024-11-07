0ce92b4462f13546ae9a2907c16e66cb73b241f3
root <root@hls-sml>
2024-11-07 15:53:02 +0100

    6-is_prime_number.c file created

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -9,18 +9,20 @@
 
 int _prime(int x, int y)
 {
-	if (y * y > x)
+	if (y == x)
 	{
 		return (1);
 	}
-	else if (x % y == 0)
+	if (x % y == 0)
 	{
 		return (0);
 	}
-	else
+	if (x % y != 0)
 	{
 		return (_prime(x, y + 1));
 	}
+
+	return (0);
 }
 
 /**
@@ -36,6 +38,12 @@ int is_prime_number(int n)
 	{
 		return (0);
 	}
-
-	return (_prime(n, 2));
+	else if (n == 0 || n == 1)
+	{
+		return (0);
+	}
+	else
+	{
+		return (_prime(n, 2));
+	}
 }
