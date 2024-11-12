b84e0e64de8e3561d1445de14399b759afd274e4
root <root@hls-sml>
2024-11-12 10:22:56 +0100

    0-malloc_checked.;c file created

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: unsigned int
+ * Return: void
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		return ((int *)98);
+	}
+
+	return (ptr);
+}
