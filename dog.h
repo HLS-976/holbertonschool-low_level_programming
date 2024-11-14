7665095a75b5d1fb4048a310c707968117ca2b23
root <root@hls-sml>
2024-11-14 17:54:23 +0100

    update file

---
+++ dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct of dog
+ * @name: char
+ * @age: float
+ * @owner: char
+ *
+ * Description: this structure
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+#endif
