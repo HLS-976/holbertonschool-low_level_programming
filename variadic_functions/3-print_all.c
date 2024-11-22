e4e9a9d8f8905fc6b3ce5f160ef850bb7ee914b8
hls <hilliassmirali@gmail.com>
2024-11-22 13:07:39 +0100

    file

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+#include <stddef.h>
+
+/**
+ * print_all - Prints anything based on a format string.
+ * @format: A list of types of arguments passed to the function.
+ */
+
+
+void print_all(const char * const format, ...)
+{}
+
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -5,4 +5,16 @@ int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+/**
+ * struct type - structure
+ * @type: type
+ * @f: function associated
+ */
+
+typedef struct type
+{
+	char *type;
+	void (*f)(char *a, 
 #endif
