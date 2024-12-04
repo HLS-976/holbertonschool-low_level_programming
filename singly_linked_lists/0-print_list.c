05486ba614d9bce1219fd24a6af8f37e8e6ed6b5
hls <hilliassmirali@gmail.com>
2024-12-04 09:52:49 +0100

    fix betty

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -8,19 +8,19 @@
 
 size_t print_list(const list_t *h)
 {
-    size_t count = 0;
-    const list_t *current = h;
+	size_t count = 0;
+	const list_t *current = h;
 
-    while (current != NULL)
-    {
-        if (current->str == NULL)
-            printf("[0] (nil)\n");
-        else
-            printf("[%u] %s\n", current->len, current->str);
+	while (current != NULL)
+	{
+		if (current->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", current->len, current->str);
 
-        count++;
-        current = current->next; // Passe au nœud suivant
-    }
+		count++;
+		current = current->next;
+	}
 
-    return count; // Retourne le nombre de nœuds
+	return (count);
 }
--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -9,11 +9,12 @@
 void free_list(list_t *head)
 {
 	list_t *node;
+	list_t *current = head;
 
-	while (head != NULL)
+	while (currrent != NULL)
 	{
-		node = head;
-		head = head->next;
+		node = current;
+		current = current->next;
 
 		if (node->str != NULL)
 			free(node->str);
