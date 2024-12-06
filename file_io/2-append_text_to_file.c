139f11c99c13d3a6eff33467380b23f84b66f591
hls <hilliassmirali@gmail.com>
2024-12-06 22:14:04 +0100

    update main.h

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -8,23 +8,29 @@
  * Return: 1 on success, -1 on failure.
  */
 
-int append_text_to_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content) 
 {
 	int fd;
-	ssize_t bytes_written;
+	ssize_t bytes_written = 0;
 
-	if (!filename)
+	if (filename == NULL)
+	{
 		return (-1);
+	}
 
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
-		return (-1);
-
-	if (text_content)
 	{
+		return (-1);
+	}
 
-		bytes_written = write(fd, text_content, sizeof(char) * strlen(text_content));
-		if (bytes_written == -1)
+	if (text_content != NULL) 
+	{
+		for (bytes_written = 0; text_content[bytes_written]; bytes_written++)
+		{
+			;
+		}
+		if (write(fd, text_content, bytes_written) == -1) 
 		{
 			close(fd);
 			return (-1);
