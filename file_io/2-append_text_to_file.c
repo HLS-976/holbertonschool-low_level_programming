7394f5d6f820cba63fb9a043af7bad547c88920c
hls <hilliassmirali@gmail.com>
2024-12-06 22:17:53 +0100

    modified file

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -8,7 +8,7 @@
  * Return: 1 on success, -1 on failure.
  */
 
-int append_text_to_file(const char *filename, char *text_content) 
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
 	ssize_t bytes_written = 0;
@@ -24,13 +24,13 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	if (text_content != NULL) 
+	if (text_content != NULL)
 	{
 		for (bytes_written = 0; text_content[bytes_written]; bytes_written++)
 		{
 			;
 		}
-		if (write(fd, text_content, bytes_written) == -1) 
+		if (write(fd, text_content, bytes_written) == -1)
 		{
 			close(fd);
 			return (-1);
