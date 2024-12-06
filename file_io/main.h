dccb8ff72995c3e244e1fa8c420ac408dbc788c5
hls-sml <hilliassmirali@gmail.com>
2024-12-06 16:13:29 +0100

    update file

--- file_io/main.h
+++ file_io/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 /*libraries*/
 #include <unistd.h>
-#include <fcnt.h>
+#include <fcntl.h>
 #include <stdlib.h>
 /*prototype*/
 ssize_t read_textfile(const char *filename, size_t letters);
